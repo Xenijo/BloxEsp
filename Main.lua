b8d4ba72fba7e3daac0dee92c6e71ab998e278d0
Xenijo <80117820+Xenijo@users.noreply.github.com>
2023-08-30 18:09:40 +0200

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,43 @@
+BloxEsp = {}
+-- services
+local runService = game:GetService("RunService");
+local players = game:GetService("Players");
+local workspace = game:GetService("Workspace");
+-- variables
+local localPlayer = players.LocalPlayer;
+local camera = workspace.CurrentCamera;
+local viewportSize = camera.ViewportSize;
+-- locals
+local Plots = {}
+local Players = {}
+local Vehicle = {}
+--Methods
+local wtvp = camera.WorldToViewportPoint;
+local isA = workspace.IsA;
+local getPivot = workspace.GetPivot;
+local findFirstChild = workspace.FindFirstChild;
+local findFirstChildOfClass = workspace.FindFirstChildOfClass;
+--//functions
+function BloxEsp:GetPlots()
+for i,v in pairs(game:GetService("Workspace").Plots:GetChildren()) do 
+table.insert(Plots,v)
+  end
+end
+
+function BloxEsp:GetPlayers()
+for i,v in pairs(game:GetService("Players"):GetPlayers()) do 
+table.insert(Players,v)
+end
+end
+
+function BloxEsp:GetVehicles()
+for i,v in pairs(game:GetService("Workspace").Plots:GetDescendants()) do 
+if v:FindFirstChild("Driver") then
+table.insert(Vehicle,v)
+end
+end
+end
+
+BloxEsp:GetVehicles()
+table.foreach(Vehicle,print)
+return BloxEsp
